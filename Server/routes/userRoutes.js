1fd1cc37ac2487d512315b00b9564d96f95f95bb
roaa12114 <roaa12114@gmail.com>
2025-10-20 02:11:54 +0200

    Handle bcrypt library

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const UserModel = require('../models/User.js')
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcryptjs");
 const dotenv = require('dotenv')
 dotenv.config()
 
